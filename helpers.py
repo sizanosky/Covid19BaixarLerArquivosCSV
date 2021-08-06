873767238f3c38845d69707b0b01eb17cf2780ae
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-08-06 04:06:10 -0300

    Update helpers.py e main.py

--- helpers.py
+++ helpers.py
@@ -1,3 +1,12 @@
+"""
+/* Helpers */
+
+Funções utilizadas em main.py
+
+Data criação: 01-08-2021
+"""
+
+
 def mens_carregando(cont=0):
     """
     Exibe uma mensagem enquanto a aplicação carrega.
@@ -11,3 +20,37 @@ def mens_carregando(cont=0):
         sleep(1)
         cont += 1
     print(f".\n")
+
+
+def pais_entrada():
+    """
+    Função que recebe a entrada do usuário e trata os dados da lista criada.
+    :return: Retorna uma lista com a entrada do usuário já tratada
+    """
+    mens_input = f"Digite o(s) nome do país(es) que você deseja obter os dados da pandemia e comparar os dados.\n"\
+                 f"Use o nome do país respeitando os identificadores universais(grafia na lingua Inglesa).\n"\
+                 f"Use apenas virgulas para separar os nomes ex: Brazil,Portugal,India.\n\n" \
+                 f"País(es): "
+    lista_pais = list(input(mens_input).split(','))
+    pais_ref = []
+
+    for pais in lista_pais:
+        pais_ref.append(pais.lstrip().title())
+
+    return pais_ref
+
+
+def data_entrada():
+    """
+    Função que recebe a entrada do usuário, cria uma lista e trata os dados da lista criada.
+    :return: Retorna uma lista com a entrada do usuário já tratada
+    :return:
+    """
+    mens_input = 'Data(s) para pesquisa (AAAA-MM-DD): '
+    lista_data = list(input(mens_input).split(','))
+    data_pesquisa = []
+
+    for data in lista_data:
+        data_pesquisa.append(data.lstrip())
+
+    return data_pesquisa
--- main.py
+++ main.py
@@ -37,26 +37,25 @@ with open('covid19.csv', 'w', newline='\n') as novo_arquivo:
 with open(ARQUIVO_CSV, 'r') as arq_obj:
     leitor_exemplo = csv.reader(arq_obj)
 
-    mens_input = f"Digite o(s) nome do país(es) que você deseja obter os dados da pandemia e comparar os dados.\n"\
-                 f"Use o nome do país respeitando os identificadores universais(grafia na lingua Inglesa).\n"\
-                 f"Use apenas virgulas para separar os nomes ex: Brazil,Portugal,India.\n\n" \
-                 f"País(es): "
-    pais_ref = list(input(mens_input).split(','))
-    data_pesquisa = input('Data da pesquisa (AAAA-MM-DD): ')
+    pais_ref = pais_entrada()
+    data_pesquisa = data_entrada()
 
     for linha in leitor_exemplo:
-        for nome_pais in pais_ref:
-            pais = nome_pais.title()
-            # Percorre o arquivo e verifica em qual linha correspondem (linha[2] = pais_ref, linha[3] =  data)
-            if linha[2] == pais and linha[3] == data_pesquisa:
+        for pais in pais_ref:
+            for data in data_pesquisa:
+                # Percorre o arquivo e verifica se há correspondência (linha[2] = pais_ref, linha[3] =  data)
+                if linha[2] == pais and linha[3] == data:
 
-                novos_casos = locale.format_string('%i', int(linha[5].removesuffix('.0')), grouping=True)
-                novas_mortes = locale.format_string('%i', int(linha[8].removesuffix('.0')), grouping=True)
-                total_casos = locale.format_string('%i', int(linha[4].removesuffix('.0')), grouping=True)
-                total_mortes = locale.format_string('%i', int(linha[7].removesuffix('.0')), grouping=True)
-                populacao = locale.format_string('%i', int(linha[44].removesuffix('.0')), grouping=True)
+                    novos_casos = locale.format_string('%i', int(linha[5].removesuffix('.0')), grouping=True)
+                    novas_mortes = locale.format_string('%i', int(linha[8].removesuffix('.0')), grouping=True)
+                    total_casos = locale.format_string('%i', int(linha[4].removesuffix('.0')), grouping=True)
+                    total_mortes = locale.format_string('%i', int(linha[7].removesuffix('.0')), grouping=True)
+                    populacao = locale.format_string('%i', int(linha[44].removesuffix('.0')), grouping=True)
+                    mortes_milhao = locale.format_string('%.1f', float(linha[13]))
+                    total_vacinados = locale.format_string('%i', int(linha[38].removesuffix('.0')), grouping=True)
 
-                print(f"\nLocal: {linha[0]}-{linha[2]} | Data: {linha[3]} | "
-                      f"Novos casos: {novos_casos} | Novas mortes: {novas_mortes} |"
-                      f"Total casos: {total_casos} | Total mortes: {total_mortes}"
-                      f"\nPopulação: {populacao}")
+                    print(f"\nLocal: {linha[0]}-{linha[2]} | Data: {linha[3]} | "
+                          f"Novos casos: {novos_casos} | Novas mortes: {novas_mortes} |"
+                          f"Total casos: {total_casos} | Total mortes: {total_mortes}"
+                          f"\nTotal de vacinados (2 doses) : {total_vacinados}"
+                          f"\nPopulação: {populacao} | Total mortes/milhão Habitantes: {mortes_milhao} m/m")
